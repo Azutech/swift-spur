5e8826ee08c0787cb30dd1efe484b17968577458
Emmanuel Onugha <azunna.onugha@gmail.com>
2023-03-02 13:14:44 +0100

    database mongoose setup

---
+++ src/connections/database.ts
@@ -0,0 +1,27 @@
+import mongoose from 'mongoose'
+import { ConnectionOptions } from 'tls';
+
+import dotenv from 'dotenv'
+
+dotenv.config()
+mongoose.set('debug', true)
+const connectionParams = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}
+
+const uri = process.env.MONGO_URI as string
+
+const database = async () => {
+    await mongoose.connect(uri, connectionParams as ConnectionOptions)
+    .then(() => {
+        console.log('Connected to Swift DB on MongoDB cluster')
+    })
+    .catch((err) => {
+        console.error(`Error connecting to the database. n${err}`);
+        process.exit(1);
+      });
+
+}
+
+export default database
