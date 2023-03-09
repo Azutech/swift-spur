9b51ccccff4966bbc691d40ca25bafea7461655b
Emmanuel Onugha <azunna.onugha@gmail.com>
2023-03-09 15:18:15 +0100

    main type setting

--- Dist/tests/databaseSpec.js
+++ Dist/tests/databaseSpec.js
@@ -1,10 +1,12 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const mongoose_1 = __importDefault(require("mongoose"));
-mongoose_1.default.Promise = global.Promise;
+'use strict'
+var __importDefault =
+    (this && this.__importDefault) ||
+    function (mod) {
+        return mod && mod.__esModule ? mod : { default: mod }
+    }
+Object.defineProperty(exports, '__esModule', { value: true })
+const mongoose_1 = __importDefault(require('mongoose'))
+mongoose_1.default.Promise = global.Promise
 // beforeAll(() => {
 //     mongoose.connect('')
 //     mongoose.connection
--- src/tests/serverSpec.ts
+++ src/tests/serverSpec.ts
@@ -1,12 +1,14 @@
-import supertest from 'supertest';
+import supertest from 'supertest'
 
-import app from '../server';
+import app from '../server'
 
 describe('Tests server connection', () => {
-  it('it expects server to be running', async () => {
-    const request = supertest(app);
-    const response = await request.get('/');
-    expect(response.status).toEqual(200);
-    expect(response.body.message).toBe('Welcome to Swift-Spur \n Lets solve your financial problems')
-  });
-});
+    it('it expects server to be running', async () => {
+        const request = supertest(app)
+        const response = await request.get('/')
+        expect(response.status).toEqual(200)
+        expect(response.body.message).toBe(
+            'Welcome to Swift-Spur \n Lets solve your financial problems'
+        )
+    })
+})
