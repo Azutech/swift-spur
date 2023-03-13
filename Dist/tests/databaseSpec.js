091fc4be707066c54abf02f0c999c456bd837751
Emmanuel Onugha <azunna.onugha@gmail.com>
2023-03-13 18:16:35 +0100

    database test test for database connection

--- Dist/tests/databaseSpec.js
+++ Dist/tests/databaseSpec.js
@@ -1,17 +1,23 @@
-'use strict'
-var __importDefault =
-    (this && this.__importDefault) ||
-    function (mod) {
-        return mod && mod.__esModule ? mod : { default: mod }
-    }
-Object.defineProperty(exports, '__esModule', { value: true })
-const mongoose_1 = __importDefault(require('mongoose'))
-mongoose_1.default.Promise = global.Promise
-// beforeAll(() => {
-//     mongoose.connect('')
-//     mongoose.connection
-//         .once('open', () =>{})
-//         .on('error', (error) => {
-//             console.warn('warning', error)
-//         })
-// })
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const mongoose_1 = __importDefault(require("mongoose"));
+const database_1 = require("../connections/database");
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
+const uri = process.env.MONGO_URI;
+describe('Mongoose Connection Test', () => {
+    beforeAll(async () => {
+        // Connect to the test database
+        await mongoose_1.default.connect(uri, database_1.connectionParams);
+    });
+    afterAll(async () => {
+        // Disconnect from the test database
+        await mongoose_1.default.disconnect();
+    });
+    it('should connect to the test database', () => {
+        expect(mongoose_1.default.connection.readyState).toEqual(2);
+    });
+});
--- Dist/tests/serverSpec.js
+++ Dist/tests/serverSpec.js
@@ -1,19 +1,15 @@
-'use strict'
-var __importDefault =
-    (this && this.__importDefault) ||
-    function (mod) {
-        return mod && mod.__esModule ? mod : { default: mod }
-    }
-Object.defineProperty(exports, '__esModule', { value: true })
-const supertest_1 = __importDefault(require('supertest'))
-const server_1 = __importDefault(require('../server'))
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const supertest_1 = __importDefault(require("supertest"));
+const server_1 = __importDefault(require("../server"));
 describe('Tests server connection', () => {
     it('it expects server to be running', async () => {
-        const request = (0, supertest_1.default)(server_1.default)
-        const response = await request.get('/')
-        expect(response.status).toEqual(200)
-        expect(response.body.message).toBe(
-            'Welcome to Swift-Spur \n Lets solve your financial problems'
-        )
-    })
-})
+        const request = (0, supertest_1.default)(server_1.default);
+        const response = await request.get('/');
+        expect(response.status).toEqual(200);
+        expect(response.body.message).toBe('Welcome to Swift-Spur \n Lets solve your financial problems');
+    });
+});
