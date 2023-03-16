ab59ca36bc8c592d2ad5e0c233d3b90fb585ec02
Emmanuel Onugha <azunna.onugha@gmail.com>
2023-03-16 14:37:03 +0100

    feature-mailService Dist folder removed test suite yet to be passed

--- src/tests/databaseSpec.ts
+++ src/tests/databaseSpec.ts
@@ -1,4 +1,32 @@
-import mongoose from 'mongoose'
+import mongoose, { ConnectOptions } from 'mongoose'
+// import { MongoMemoryServer } from 'mongodb-memory-server'
+
+// describe('Database Connection Test', () => {
+//     let mongoServer: any
+
+//     beforeAll(async () => {
+//         mongoServer = new MongoMemoryServer()
+
+//         await mongoServer.start()
+//         const uri = mongoServer.getUri()
+//         await mongoose.connect(uri, {
+//             useNewUrlParser: true,
+//             useCreateIndex: true,
+//             useUnifiedTopology: true,
+//         } as ConnectOptions)
+        
+//     })
+
+//     afterAll(async () => {
+//         await mongoose.disconnect()
+//         await mongoServer.stop()
+//     })
+
+//     it('should connect to the test database', () => {
+//         expect(mongoose.connection.readyState).toEqual(1)
+//     })
+// })
+
 import { connectionParams } from '../connections/database'
 import { ConnectionOptions } from 'tls'
 import dotenv from 'dotenv'
