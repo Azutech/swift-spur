df80c55702b1bcf3a84c617cdec1c2d085bf6528
Emmanuel Onugha <azunna.onugha@gmail.com>
2023-03-22 19:44:18 +0100

    docker container running

--- Dist/src/logger/customLog.js
+++ Dist/src/logger/customLog.js
@@ -1,23 +1,33 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const winston_1 = require("winston");
-const { combine, timestamp, label, printf, colorize, simple } = winston_1.format;
+'use strict'
+Object.defineProperty(exports, '__esModule', { value: true })
+const winston_1 = require('winston')
+const { combine, timestamp, label, printf, colorize, simple } = winston_1.format
 const myFormat = printf(({ level, message, label, timestamp }) => {
-    return `${timestamp} [${label}] ${level}: ${message}`;
-});
-const labelTimestamps = combine(label({ label: 'boom' }), timestamp(), myFormat);
+    return `${timestamp} [${label}] ${level}: ${message}`
+})
+const labelTimestamps = combine(label({ label: 'boom' }), timestamp(), myFormat)
 const logger = (0, winston_1.createLogger)({
     level: 'debug',
     format: labelTimestamps,
     exitOnError: false,
     transports: [
         new winston_1.transports.Console({
-            format: combine(label({ message: true, label: 'MANGA EXPRESS SERVER!' }), colorize(), simple()),
+            format: combine(
+                label({ message: true, label: 'MANGA EXPRESS SERVER!' }),
+                colorize(),
+                simple()
+            ),
         }),
-        new winston_1.transports.File({ filename: 'src/logs/error.log', level: 'error' }),
-        new winston_1.transports.File({ filename: 'src/logs/info.log', level: 'info' }),
+        new winston_1.transports.File({
+            filename: 'src/logs/error.log',
+            level: 'error',
+        }),
+        new winston_1.transports.File({
+            filename: 'src/logs/info.log',
+            level: 'info',
+        }),
         new winston_1.transports.File({ filename: 'src/logs/debug.log' }),
         new winston_1.transports.File({ filename: 'src/logs/combined.log' }),
     ],
-});
-exports.default = logger;
+})
+exports.default = logger
