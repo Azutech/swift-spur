53e2a186986460a86bf55d1e9db97e29cb0a1f91
Emmanuel Onugha <azunna.onugha@gmail.com>
2023-03-22 19:46:39 +0100

    docker Dist folder removed

--- Dist/config/custom-env.js
+++
@@ -1,6 +0,0 @@
-'use strict'
-Object.defineProperty(exports, '__esModule', { value: true })
-exports.default = {
-    tokenPrivatekey: 'TOKEN_PRIVATE_KEY',
-    tokenPublickey: 'TOKEN_PUBLIC_KEY',
-}
--- Dist/config/default.js
+++
@@ -1,7 +0,0 @@
-'use strict'
-Object.defineProperty(exports, '__esModule', { value: true })
-exports.default = {
-    port: 3000,
-    salt: 10,
-    accessTokenExpiresIn: 15,
-}
