a65433a098a0db24285b7f763d9c334593e35c76
Emmanuel Onugha <azunna.onugha@gmail.com>
2023-03-27 11:50:21 +0100

    kyc address Schema added

---
+++ src/models/kyc.ts
@@ -0,0 +1,25 @@
+import mongoose from "mongoose"
+
+
+
+export const AddressSchema = new mongoose.Schema(
+    {
+      streetAdress: {
+        type: String,
+      },
+      city: {
+        type: String,
+      },
+      state: {
+        type: String,
+      },
+      country: {
+        type: String,
+      },
+      zipcode: {
+        type: String,
+      },
+    },
+    { _id: false }
+  );
+  
