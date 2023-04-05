2554d24296b492faf0748dedd783565d14d46b17
Emmanuel Onugha <azunna.onugha@gmail.com>
2023-04-05 15:21:19 +0100

    loans
    Chore: loans model created

---
+++ src/models/loans.ts
@@ -0,0 +1,32 @@
+import mongoose, {Schema, model} from 'mongoose'
+import { User } from './users'
+import { required } from 'joi'
+
+
+
+const loanSchema = new Schema({
+
+    userId : {
+        type: Schema.Types.ObjectId,
+        required: true,
+        ref: User
+    },
+
+    loanApproved : {
+        type : Boolean,
+        default: false
+    },
+
+    loanEligiblity : {
+        type: Boolean,
+        required: true,
+        default: false
+    },
+
+    loanAmount : {
+        type: Number,
+        required: true
+    }
+},   { timestamps: true })
+
+export const Loans = model('loans', loanSchema)
