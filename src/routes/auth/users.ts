caeabcdade4f5bb04901e442f393c6eed1db3ccf
Emmanuel Onugha <azunna.onugha@gmail.com>
2023-06-05 18:42:24 +0100

    [feature]: forgot password api ready

--- src/routes/auth/users.ts
+++ src/routes/auth/users.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express'
 import { validate } from '../../middleware/validate'
 import { validateUser } from '../../models/users'
-import { register, authenticate } from '../../services/auth/users'
+import { register, authenticate, forgotpass } from '../../services/auth/users'
 
 export const user = Router()
 
 user.post('/auth/sign-up', [validate(validateUser)], register)
 user.post('/auth/login', authenticate)
+user.post('/auth/forgotpass', forgotpass)
